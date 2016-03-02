9f5660b38dc6dacd5c7e304ddc0eb86874a3b905
328918412 <328918412@qq.com>
2016-03-02 09:54:27 +0800

    WeChatDemo

---
+++ WeChat/src/po/NewsMessage.java
@@ -0,0 +1,25 @@
+package po;
+
+import java.util.List;
+
+public class NewsMessage extends BaseMessage {
+	private int ArticleCount;// 消息数
+	private List<News> Articles;// 消息体
+
+	public int getArticleCount() {
+		return ArticleCount;
+	}
+
+	public void setArticleCount(int articleCount) {
+		ArticleCount = articleCount;
+	}
+
+	public List<News> getArticles() {
+		return Articles;
+	}
+
+	public void setArticles(List<News> articles) {
+		Articles = articles;
+	}
+
+}
